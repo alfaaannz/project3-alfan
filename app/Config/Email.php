94b46de0b8fa1c7f574575f5492a2db7a166a06b
Muhammad Alfan Alfajri <muhammadalfan2304@gmail.com>
2024-08-08 18:37:24 +0700

    editing source

--- app/Config/Email.php
+++ app/Config/Email.php
@@ -6,116 +6,33 @@ use CodeIgniter\Config\BaseConfig;
 
 class Email extends BaseConfig
 {
-    public string $fromEmail  = 'fajrimaulanacml@gmail.com';
-    public string $fromName   = 'fajrimaulana';
+    public string $fromEmail = 'muhammadalfan2304@gmail.com';
+    public string $fromName = 'muhamadalfan';
     public string $recipients = '';
 
-    /**
-     * The "user agent"
-     */
     public string $userAgent = 'CodeIgniter';
 
-    /**
-     * The mail sending protocol: mail, sendmail, smtp
-     */
-    public string $protocol = 'mail';
+    public string $protocol = 'smtp'; // Menggunakan SMTP
 
-    /**
-     * The server path to Sendmail.
-     */
-    public string $mailPath = '/usr/sbin/sendmail';
+    public string $mailPath = ''; // Kosongkan jika menggunakan SMTP
 
-    /**
-     * SMTP Server Hostname
-     */
-    public string $SMTPHost = '';
+    public string $SMTPHost = 'live.smtp.mailtrap.io'; // Host SMTP Anda
+    public string $SMTPUser = 'api';       // Username SMTP Anda
+    public string $SMTPPass = '481d846e8593f2537137ada6c4c76e92';           // Password SMTP Anda
+    public int $SMTPPort = 587;                                 // Port SMTP
+    public string $SMTPCrypto = 'tls';                           // Enkripsi SMTP
 
-    /**
-     * SMTP Username
-     */
-    public string $SMTPUser = '';
-
-    /**
-     * SMTP Password
-     */
-    public string $SMTPPass = '';
-
-    /**
-     * SMTP Port
-     */
-    public int $SMTPPort = 25;
-
-    /**
-     * SMTP Timeout (in seconds)
-     */
-    public int $SMTPTimeout = 5;
-
-    /**
-     * Enable persistent SMTP connections
-     */
-    public bool $SMTPKeepAlive = false;
-
-    /**
-     * SMTP Encryption.
-     *
-     * @var string '', 'tls' or 'ssl'. 'tls' will issue a STARTTLS command
-     *             to the server. 'ssl' means implicit SSL. Connection on port
-     *             465 should set this to ''.
-     */
-    public string $SMTPCrypto = 'tls';
-
-    /**
-     * Enable word-wrap
-     */
     public bool $wordWrap = true;
-
-    /**
-     * Character count to wrap at
-     */
     public int $wrapChars = 76;
-
-    /**
-     * Type of mail, either 'text' or 'html'
-     */
-    public string $mailType = 'text';
-
-    /**
-     * Character set (utf-8, iso-8859-1, etc.)
-     */
+    public string $mailType = 'html'; // Menggunakan HTML
     public string $charset = 'UTF-8';
+    public bool $validate = true;
 
-    /**
-     * Whether to validate the email address
-     */
-    public bool $validate = false;
-
-    /**
-     * Email Priority. 1 = highest. 5 = lowest. 3 = normal
-     */
     public int $priority = 3;
-
-    /**
-     * Newline character. (Use “\r\n” to comply with RFC 822)
-     */
     public string $CRLF = "\r\n";
-
-    /**
-     * Newline character. (Use “\r\n” to comply with RFC 822)
-     */
     public string $newline = "\r\n";
 
-    /**
-     * Enable BCC Batch Mode.
-     */
     public bool $BCCBatchMode = false;
-
-    /**
-     * Number of emails in each BCC batch
-     */
     public int $BCCBatchSize = 200;
-
-    /**
-     * Enable notify message from server
-     */
     public bool $DSN = false;
 }
